08912cd649db9247f478b61fca8a103201d27163
Tomasz Bogusz <tomo.bogusz@gmail.com>
2018-04-05 23:38:29 +0200

    Add convenience extension to handle NavigationController contents

--- Cassini/Controller/CassiniViewController.swift
+++ Cassini/Controller/CassiniViewController.swift
@@ -20,16 +20,22 @@ class CassiniViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let identifier = segue.identifier {
             if let url = DemoURLs.NASA[identifier] {
-                var destination = segue.destination
-                if let navcon = destination as? UINavigationController {
-                    destination = navcon.visibleViewController ?? navcon
-                }
-                if let imageVC = destination as? ImageViewController {
+                if let imageVC = segue.destination.contents as? ImageViewController {
                     imageVC.imageURL = url
                     imageVC.title = (sender as? UIButton)?.currentTitle
                 }
             }
         }
     }
+}
 
+// MARK: - Convenience extension to return contents if NavigationController
+extension UIViewController {
+    var contents: UIViewController {
+        if let navcon = self as? UINavigationController {
+            return navcon.visibleViewController ?? self
+        } else {
+            return self
+        }
+    }
 }
