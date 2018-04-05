f71c598e331676df93caaa209c3f82c1e9f2d67d
Tomasz Bogusz <tomo.bogusz@gmail.com>
2018-04-05 14:45:50 +0200

    Implement load image logic

--- Cassini/Controller/ImageViewController.swift
+++ Cassini/Controller/ImageViewController.swift
@@ -9,6 +9,17 @@
 import UIKit
 
 class ImageViewController: UIViewController {
+    
+    // MARK: - Model
+    var imageURL: URL? {
+        didSet {
+            imageView.image = nil
+            // Checki if I'm on screen
+            if view.window != nil {
+                fetchImage()
+            }
+        }
+    }
 
     // MARK: - Outlets
     @IBOutlet weak var imageView: UIImageView!
@@ -16,6 +27,26 @@ class ImageViewController: UIViewController {
     // MARK: - View Controller's Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        if imageURL == nil {
+            imageURL = DemoURLs.stanford
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if imageView.image == nil {
+            fetchImage()
+        }
+    }
+    
+    // MARK: - Private functions
+    private func fetchImage() {
+        if let url = imageURL {
+            let urlContents = try? Data(contentsOf: url)
+            if let imageData = urlContents {
+                imageView.image = UIImage(data: imageData)
+            }
+        }
     }
 
 }
