ceb7c3eae127125b24475d089ff2424ab62f59b8
Tomasz Bogusz <tomo.bogusz@gmail.com>
2018-04-05 13:58:36 +0200

    Setup project from scratch

---
+++ Cassini/Controller/ImageViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ImageViewController.swift
+//  Cassini
+//
+//  Created by Tomasz Bogusz on 05.04.2018.
+//  Copyright © 2018 Tomasz Bogusz. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
